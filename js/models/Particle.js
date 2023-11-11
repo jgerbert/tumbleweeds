a2ad0bfdadb04be765494c6b9cd03df88c5ecca4
“jgerbert” <gerbert@gmail.comgit config --global user.email gerbert@gmail.comgit config --global user.email gerbert@gmail.com>
2023-11-11 14:38:00 -0800

    constants added

--- js/models/Particle.js
+++ js/models/Particle.js
@@ -1,3 +1,42 @@
+// Particle Configuration Constants
+
+// Minimum and maximum duration for particle's fade-in effect in milliseconds.
+const PARTICLE_FADE_IN_DURATION_MIN = 0;
+const PARTICLE_FADE_IN_DURATION_MAX = 750;
+
+// Minimum and maximum start time for particle's fade-out effect in milliseconds.
+const PARTICLE_FADE_OUT_START_MIN = 1000;
+const PARTICLE_FADE_OUT_START_MAX = 3000;
+
+// Duration for particle's fade-out effect in milliseconds.
+const PARTICLE_FADE_OUT_DURATION = 5000;
+
+// Minimum and maximum size of the particle.
+const PARTICLE_SIZE_MIN = 0.8;
+const PARTICLE_SIZE_MAX = 14.2;
+
+// Base and range for calculating the opacity factor of the particle.
+const PARTICLE_OPACITY_FACTOR_BASE = 0.5;
+const PARTICLE_OPACITY_FACTOR_RANGE = 0.5;
+
+// Minimum and maximum number of sides for the particle shape.
+const PARTICLE_SIDES_MIN = 4;
+const PARTICLE_SIDES_MAX = 6;
+
+// Minimum and maximum rotation speed of the particle.
+const PARTICLE_ROTATION_SPEED_MIN = -0.005;
+const PARTICLE_ROTATION_SPEED_MAX = 0.005;
+
+// Minimum and maximum lifespan of the particle in milliseconds.
+const PARTICLE_LIFESPAN_MIN = 2000;
+const PARTICLE_LIFESPAN_MAX = 5000;
+
+// Easing factor for particle movement, influencing how quickly it responds to forces.
+const PARTICLE_EASING = 0.3;
+
+// Factor for random turbulence effect applied to particle's velocity.
+const PARTICLE_TURBULENCE_FACTOR = 0.07;
+
 class Particle {
   // The Particle constructor initializes a particle with its position, velocity, thrust time, and other properties.
   constructor(x, y, angle, magnitude, thrustTime) {
@@ -23,30 +62,26 @@ class Particle {
 
   // Set the visual appearance properties of the particle.
   initAppearance() {
-      // Time for the particle to fade in.
-      this.fadeInDuration = getRandomFadeInDuration();
-      // Time after which the particle starts to fade out.
-      this.fadeOutStart = this.thrustTime + getRandomFadeOutStart();
-      this.fadeOutDuration = 5000;  // Duration for the particle to fade out
-      this.fadeInStart = performance.now();  // Timestamp marking the start of the fade-in
-      this.opacity = 0;  // Initial opacity of the particle
-      this.color = getRandomColor();  // Random color for the particle
-      this.size = getRandomSize();  // Random size for the particle
-      this.opacityFactor = getOpacityFactor(this.size);  // Opacity adjustment based on size
-  }
+    this.fadeInDuration = getRandomFadeInDuration();
+    this.fadeOutStart = this.thrustTime + getRandomFadeOutStart();
+    this.fadeOutDuration = PARTICLE_FADE_OUT_DURATION;
+    this.fadeInStart = performance.now();
+    this.opacity = 0;
+    this.color = getRandomColor();
+    this.size = getRandomSize();
+    this.opacityFactor = getOpacityFactor(this.size);
+}
 
   // Initialize properties related to the particle's movement.
   initMovement() {
-      this.easing = 0.3;  // Easing factor for particle movement
-      // Speed adjustment based on the particle's opacity factor.
-      this.speedFactor = 1 + this.opacityFactor;  
-      // Total lifespan of the particle.
-      this.lifespan = 2000 + Math.random() * 3000;
-      this.sides = getRandomSides();  // Number of sides for the particle shape
-      this.angles = getAngles(this.sides);  // Angles based on the number of sides
-      this.rotation = 0;  // Initial rotation angle
-      this.rotationSpeed = getRandomRotationSpeed();  // Speed at which the particle rotates
-  }
+    this.easing = PARTICLE_EASING;
+    this.speedFactor = 1 + this.opacityFactor;
+    this.lifespan = PARTICLE_LIFESPAN_MIN + Math.random() * (PARTICLE_LIFESPAN_MAX - PARTICLE_LIFESPAN_MIN);
+    this.sides = getRandomSides();
+    this.angles = getAngles(this.sides);
+    this.rotation = 0;
+    this.rotationSpeed = getRandomRotationSpeed();
+}
 
   // Update the particle's position and opacity.
   update(deltaTime, windForce) {
@@ -78,6 +113,35 @@ class Particle {
 // --------------------
 // HELPER FUNCTIONS
 // --------------------
+// 
+// Returns a random duration (in milliseconds) for the particle's fade-in effect.
+function getRandomFadeInDuration() {
+  return Math.random() * (PARTICLE_FADE_IN_DURATION_MAX - PARTICLE_FADE_IN_DURATION_MIN) + PARTICLE_FADE_IN_DURATION_MIN;
+}
+
+// Returns a random start time (in milliseconds) for the particle's fade-out effect.
+function getRandomFadeOutStart() {
+  return Math.random() * (PARTICLE_FADE_OUT_START_MAX - PARTICLE_FADE_OUT_START_MIN) + PARTICLE_FADE_OUT_START_MIN;
+}
+
+// Returns a random size factor for the particle.
+function getRandomSize() {
+  return Math.random() * (PARTICLE_SIZE_MAX - PARTICLE_SIZE_MIN) + PARTICLE_SIZE_MIN;
+}
+
+// Calculates and returns an opacity factor based on the particle's size.
+function getOpacityFactor(size) {
+  return (size / 10) * (PARTICLE_OPACITY_FACTOR_BASE + Math.random() * PARTICLE_OPACITY_FACTOR_RANGE);
+}
+
+// Returns a random number of sides for the particle (between 4 and 6 inclusive).
+function getRandomSides() {
+  return Math.floor(Math.random() * (PARTICLE_SIDES_MAX - PARTICLE_SIDES_MIN + 1)) + PARTICLE_SIDES_MIN;
+}
+
+function getRandomRotationSpeed() {
+  return Math.random() * (PARTICLE_ROTATION_SPEED_MAX - PARTICLE_ROTATION_SPEED_MIN) + PARTICLE_ROTATION_SPEED_MIN;
+}
 
 // Returns a random primary color from a predefined list.
 function getRandomColor() {
@@ -95,31 +159,6 @@ function getRandomColor() {
   return primaryColors[Math.floor(Math.random() * primaryColors.length)].hex;
 }
 
-// Returns a random duration (in milliseconds) for the particle's fade-in effect.
-function getRandomFadeInDuration() {
-  return Math.random() * 750;
-}
-
-// Returns a random start time (in milliseconds) for the particle's fade-out effect.
-function getRandomFadeOutStart() {
-  return Math.random() * 2000 + 1000;
-}
-
-// Returns a random size factor for the particle.
-function getRandomSize() {
-  return Math.random() * 14.2 + .8;
-}
-
-// Calculates and returns an opacity factor based on the particle's size.
-function getOpacityFactor(size) {
-  return (size / 10) * (0.5 + Math.random() * 0.5);
-}
-
-// Returns a random number of sides for the particle (between 4 and 6 inclusive).
-function getRandomSides() {
-  return Math.floor(Math.random() * 3) + 4;
-}
-
 // Calculates and returns the angles for a polygon based on the number of sides.
 function getAngles(sides) {
   const angles = [];
@@ -128,11 +167,6 @@ function getAngles(sides) {
       angles.push((Math.PI * 2 * i) / sides + (Math.random() * angleOffset - angleOffset / 2));
   }
   return angles;
-}
-
-// Returns a random rotation speed for the particle.
-function getRandomRotationSpeed() {
-  return Math.random() * 0.01 - 0.005;
 }
 
 // Applies a random turbulence effect to the particle's velocity.
@@ -150,7 +184,6 @@ function wrapAroundScreen(particle) {
   if (particle.y > window.innerHeight) particle.y -= window.innerHeight;
 }
 
-// Updates the particle's velocity based on the wind force and then moves the particle.
 // Updates the particle's velocity based on the wind force and then moves the particle.
 function updateVelocityBasedOnWind(particle, windForce, deltaTime) {
   // Calculate the wind's angle and magnitude.
